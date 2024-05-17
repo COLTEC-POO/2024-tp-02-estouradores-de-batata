948f6c5b184a037f5bb85c516590c0e382d98411
Fonseca <julia.fsc.barbosa@gmail.com>
2024-05-17 09:55:11 -0300

    pedro consertos

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 import java.util.ArrayList;
 
-public abstract class Usuario {
+public class Usuario {
 
     String nome;
     String cpf;
@@ -19,8 +19,8 @@ public abstract class Usuario {
     boolean devolverLivro(Livro livro) {
         boolean devolvido = false;
         if(livrosAlugados.contains(livro)) {
-            livro.setQuantidade(1);
             livrosAlugados.remove(livro);
+            livro.numeroDeEmprestimos --;
             devolvido = true;
         }
         return devolvido;
@@ -28,36 +28,21 @@ public abstract class Usuario {
 
     boolean pegarLivro(Livro livro) {
         boolean pego = false;
-        if(livro.quantidade > 0) {
-            livro.setQuantidade(-1);
+        if(livro.quantidade - livro.numeroDeEmprestimos > 0) {
             livrosAlugados.add(livro);
+            livro.numeroDeEmprestimos ++;
             pego = true;
         }
-        else if(livro.quantidade == 0) {
-            System.out.println("Todas as copias ja foram alugadas...");
-        } else
-            System.out.println("Não temos esse livro ou não existe...");
+        else
+            System.out.println("Livro indisponível");
         return pego;
     }
 
-    void imprimirLivros(boolean amount){
-        System.out.println("-------Livros Alugados-------");
+    void imprimirLivros(){
+        System.out.println("Livros Alugados:\n");
         for(Livro livro : livrosAlugados){
-            livro.imprimir(amount);
+            livro.imprimir();
         }
-        System.out.println("-----------------------------");
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public int getId() {
-        return id;
     }
 
 }
